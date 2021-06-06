826a219c43eccf8565036c17db0fbcace0cf5c91
Andrew McIndoe <amcindoe@powerhrg.com>
2021-06-06 15:41:10 -0400

    Toy Tale Lab

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 let addToy = false;
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+const toyCollection = document.querySelector('#toy-collection')
+const createToyForm = document.querySelector('.add-toy-form')
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -13,3 +15,51 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const initialize = () => {
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(toys => toys.forEach(showToy))
+}
+
+const showToy = toy => {
+  let toyNew = toy
+  let toyCard = document.createElement('div')
+  toyCard.className = 'card'
+  toyCard.innerHTML = `
+  <h2>${toyNew.name}</h2>
+  <img src=${toyNew.image} class="toy-avatar" />`
+  let likesP = document.createElement('p')
+  likesP.innerText = `${toyNew.likes} Likes`
+  toyCard.append(likesP)
+  let likeBtn = document.createElement('button')
+  likeBtn.className = 'like-btn'
+  likeBtn.innerText = 'Like <3'
+  likeBtn.addEventListener('click', e => increaseLikes(e, toyNew))
+  toyCard.append(likeBtn)
+  toyCollection.append(toyCard)
+}
+
+const increaseLikes = (e, toy) => {
+  let toyLikes = toy.likes
+  let toyId = toy.id
+  return fetch(`http://localhost:3000/toys/${toyId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({ likes: ++toyLikes })
+  })
+    .then(resp => resp.json())
+    .then(showLikes(e))
+}
+
+const showLikes = e => {
+  e.preventDefault()
+  let newNum =
+    parseInt(e.target.parentElement.children[2].innerText.split(' ')[0]) + 1
+  e.target.parentElement.children[2].innerText = `${newNum} Likes`
+}
+
+initialize()
